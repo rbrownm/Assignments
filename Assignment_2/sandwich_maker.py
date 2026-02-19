4d85634d116ef44a6543c0f19ed973fa7d40179f
Ryan Brown <rbrow221@charlotte.edu>
2026-02-19 17:23:45 -0500

    feat: added functionality for check_resources and make_sandwich

--- Assignment_2/sandwich_maker.py
+++ Assignment_2/sandwich_maker.py
@@ -5,7 +5,20 @@ class SandwichMaker:
 
     def check_resources(self, ingredients):
         """Returns True when order can be made, False if ingredients are insufficient."""
-        #####
+        if self.machine_resources['bread'] < ingredients['bread']:
+            print(f"Sorry there is not enough bread")
+            return False
+        if self.machine_resources['ham'] < ingredients['ham']:
+            print(f"Sorry there is not enough ham")
+            return False
+        if self.machine_resources['cheese'] < ingredients['cheese']:
+            print(f"Sorry there is not enough cheese")
+            return False
+
+        return True
 
     def make_sandwich(self, sandwich_size, order_ingredients):
-        ########
+
+        self.machine_resources['bread'] -= order_ingredients['bread']
+        self.machine_resources['ham'] -= order_ingredients['ham']
+        self.machine_resources['cheese'] -= order_ingredients['cheese']
