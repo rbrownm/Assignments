9d1c4ae96ca03bf8ef7a0648540a43cc9964e583
Ryan Brown <rbrow221@charlotte.edu>
2026-02-28 15:22:12 -0500

    feat: added create, read_all, read_one functionality

--- Assignment_5/api/controllers/order_details.py
+++ Assignment_5/api/controllers/order_details.py
@@ -1,3 +1,24 @@
 from sqlalchemy.orm import Session
 from fastapi import HTTPException, status, Response, Depends
 from ..models import models, schemas
+
+
+def create (db: Session, order_details):
+    db_order_detail = models.OrderDetail(
+    order_id = order_details.order_id,
+    sandwich_id = order_details.sandwich_id,
+    amount = order_details.amount
+    )
+
+    db.add(db_order_detail)
+    db.commit()
+    db.refresh(db_order_detail)
+    return db_order_detail
+
+def read_all(db: Session):
+    return db.query(models.OrderDetail).all()
+
+def read_one(db: Session, order_detail_id: int):
+    return db.query(models.OrderDetail).filter(models.OrderDetail.id == order_detail_id).first()
+
+
